f0ad7b751b17ae1692601a6d7fe00c6145670060
auhpp <hpphiau@gmail.com>
2025-10-20 10:25:55 +0700

    Cai dat du an

---
+++ app/config/index.js
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+
+const config = {
+    app: {
+        port: process.env.PORT || 3000
+    },
+    db: {
+        uri: process.env.MONGODB_URI || "mongodb://localhost:27017/book_lending"
+    }
+}
+
+export default config;
